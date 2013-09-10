8961e23d268021652ae68661e7525672c93e18f4
Cefn Hoile <github.com_cefn@cefn.com>
2013-09-10 09:11:08 +0100

    Launch script to introspect window containing notes, and launch a window containing the paper

---
+++ view_noted_paper.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+SCRIPT=`realpath "$0"`
+SCRIPTPATH=`dirname "$SCRIPT"`
+cd "$SCRIPTPATH"
+FILEID=$(xdotool search --name "BlueGriffon" | tail -n1 | xargs -n1 xdotool getwindowname | grep --text --only-matching --perl-regexp '(?<=/)[^/]*(?=\.html)' )
+evince "papers/$FILEID.pdf"
