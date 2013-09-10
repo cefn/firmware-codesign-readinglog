d933403ece1f19764819f336f0d6eef593c9ad20
Cefn Hoile <github.com_cefn@cefn.com>
2013-09-10 09:15:07 +0100

    Added 'force' flag

--- log_paper.sh
+++ log_paper.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 SCRIPT=`realpath "$0"`
 SCRIPTPATH=`dirname "$SCRIPT"`
+
+# check if forced
+if [ "$1" == "-y" ] 
+	then
+		ASK=0
+		shift
+	else
+		ASK=1
+fi
+
 FILE=`realpath "$1"`
 evince "$FILE" &
 disown
@@ -13,24 +23,24 @@ TITLE=`java -jar docears-pdf-inspector.jar -title "$FILE"`
 # workaround to force Zenity to appear on top
 WINDOWID=
 # ask user to log or not
-if zenity --question --text="Log $TITLE for PhD?"; then
-  # user wants this document to be logged
-  
-  # figure out unique identifier and reading log file for the document
-  HASH=`md5sum "$FILE" | cut -d " " -f1`
-  BACKUPFILE="papers/${HASH}.pdf"
-  LOGFILE="notes/${HASH}.html"
-  
-  cp "$FILE" "$BACKUPFILE"
+if [ $ASK -eq 0 ] || zenity --question --text="Log $TITLE for PhD?" 
+	then
+		# user wants this document to be logged
+		  
+		# figure out unique identifier and reading log file for the document
+		HASH=`md5sum "$FILE" | cut -d " " -f1`
+		BACKUPFILE="papers/${HASH}.pdf"
+		LOGFILE="notes/${HASH}.html"
+		  
+		cp "$FILE" "$BACKUPFILE"
 
-  # create log file if it doesn't exist, expanding BASH variables inline
-  if [ ! -f "$LOGFILE" ]; then
-      echo "Creating new log file $LOGFILE"
-      echo $(eval echo $(cat notes_template.html)) >> "$LOGFILE"
-  fi
+		# create log file if it doesn't exist, expanding BASH variables inline
+		if [ ! -f "$LOGFILE" ]; then
+		  echo "Creating new log file $LOGFILE"
+		  echo $(eval echo $(cat notes_template.html)) >> "$LOGFILE"
+		fi
 
-  # load the log file for editing
-  bluegriffon "`realpath $LOGFILE`" &
-  disown
-    
+		# load the log file for editing
+		bluegriffon "`realpath $LOGFILE`" &
+		disown    
 fi
