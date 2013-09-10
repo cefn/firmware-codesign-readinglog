e37ea92997754db313432e1b4772bc467c7850f9
Cefn Hoile <github.com_cefn@cefn.com>
2013-09-10 09:16:43 +0100

    Made load and save asymmetrical. Added Nohup.

--- load_evince.sh
+++ load_evince.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find /home/cefn/Desktop/tmp_pdfs/ -type f -name '*.pdf' -print0 | xargs -n1 -0 log_paper.sh
+find /home/cefn/Desktop/pdfs_to_load/ -type f -name '*.pdf' -print0 | nohup xargs --max-procs=0 -n1 -0 evince
--- snapshot_evince.sh
+++ snapshot_evince.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkdir -p ~/Desktop/tmp_pdfs
-ps aux | grep -v '*' | grep -o -E '/home/cefn.*\.pdf' | xargs -n1 -I"{}" cp {} ~/Desktop/tmp_pdfs/
+mkdir -p ~/Desktop/pdfs_saved
+ps aux | grep -v '*' | grep -o -E '/home/cefn.*\.pdf' | xargs -n1 -I"{}" cp {} ~/Desktop/pdfs_saved/
