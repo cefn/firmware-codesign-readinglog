1b79b29aaac93a9fbfa55fba1398e6980827abd6
Cefn Hoile <github.com_cefn@cefn.com>
2013-09-18 13:10:26 +0100

    Established more effective syncing between save and load directories

--- snapshot_evince.sh
+++ snapshot_evince.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 mkdir -p ~/Desktop/pdfs_saved
-ps aux | grep -v '*' | grep -o -E '/home/cefn.*\.pdf' | xargs -n1 -I"{}" cp {} ~/Desktop/pdfs_saved/
+rm -f ~/Desktop/pdfs_saved/*
+ps aux | grep -v '*' | grep -o -P '(?<=evince).*/.*\.pdf' | xargs -n1 -I"{}" cp "{}" ~/Desktop/pdfs_saved/
+rsync --archive --whole-file --recursive --delete ~/Desktop/pdfs_saved/ ~/Desktop/pdfs_to_load/
