d6e0d33f53ab761e24a0a6ce894e4cbe8a2a1f77
Cefn Hoile <github.com_cefn@cefn.com>
2013-09-20 11:33:51 +0100

    Moved to script/sync_data.sh and function changed substantially for new zorba-based python+xquery server.

--- prep_data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-SCRIPT=`realpath "$0"`
-SCRIPTPATH=`dirname "$SCRIPT"`
-cd "$SCRIPTPATH"
-cat notes/*.html > all.html
-tidy -im -quiet -asxhtml -utf8 -numeric --show-warnings false --tidy-mark no --doctype omit --wrap 0 all.html
-#basexgui
