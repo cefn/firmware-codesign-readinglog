375db82ddcae1de9d5417dbff6be0c7761e472db
Cefn Hoile <github.com_cefn@cefn.com>
2013-09-20 11:30:42 +0100

    Removed spurious scripts - since superceded or renamed

--- scripts/report.py
+++
@@ -1,6 +0,0 @@
-import zorba_api
-store = zorba_api.InMemoryStore_getInstance()
-zorba = zorba_api.Zorba_getInstance(store)
-
-with open('report.xq', 'r') as xquery:
-    print zorba.compileQuery(xquery.read()).execute()
