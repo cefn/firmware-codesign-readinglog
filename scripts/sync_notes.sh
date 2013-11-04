9eb8ab11e5dec7223e8f7ffc2c0edd6d282b84ee
Cefn Hoile <github.com@cefn.com>
2013-11-04 11:46:32 +0000

    Added reporting of individual files as they are copied and tidied. Fixed some escaping issues.

--- scripts/sync_notes.sh
+++ scripts/sync_notes.sh
@@ -7,4 +7,4 @@ cd "$SCRIPTPATH"
 mkdir -p ${TODIR}
 #rsync --exclude=".gitignore" --recursive --whole-file --delete ${FROMDIR} ${TODIR} # lossy version which deletes
 rsync --exclude=".gitignore" --recursive --whole-file ${FROMDIR} ${TODIR}
-tidy -im -quiet -asxhtml -numeric  --input-encoding win1252 --output-encoding utf8 --indent yes --vertical-space no --show-warnings false --tidy-mark no --drop-empty-paras no --enclose-text yes --doctype omit --wrap 80 ${TODIR}*.html
+find ${TODIR} -print0 -name '*.html' | xargs -n1 -0 -I '{}' sh -c "echo {} && tidy -im -quiet -asxhtml -numeric  --input-encoding win1252 --output-encoding utf8 --indent yes --vertical-space no --show-warnings false --tidy-mark no --drop-empty-paras no --enclose-text yes --doctype omit --wrap 80 {}"
