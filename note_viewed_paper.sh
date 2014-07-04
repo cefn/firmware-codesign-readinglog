627761001d14b6403a968b102b6eeb289b3f7ef8
Cefn Hoile <github.com@cefn.com>
2014-07-04 15:22:33 +0100

    Generalised xdotool search query to make testing easier. Added routine to remove evince invocation from introspected command line.

--- note_viewed_paper.sh
+++ note_viewed_paper.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+# depends on previous apt-get installation of 'xdotool'
+QUERY="getwindowfocus"
+#QUERY="search landscapes"
+#QUERY="search PDC2010"
+#QUERY="search bf19"
 SCRIPT=`realpath "$0"`
 SCRIPTPATH=`dirname "$SCRIPT"`
+# makes sure that papers/xxx.pdf references are valid
 cd "$SCRIPTPATH"
-FILE=$(cat /proc/"$(xdotool getwindowpid $(xdotool getwindowfocus))"/cmdline | xargs -0 echo | grep --text --only-matching --extended-regexp '/.*.pdf$')
+FILE=$(cat /proc/"$(xdotool getwindowpid $(xdotool ${QUERY} 2>&1 |tail -n1))"/cmdline | xargs -0 echo | grep --text --only-matching --extended-regexp '.*/.*.pdf$' | sed 's/\.*evince //g')
 log_paper.sh -y "$FILE"
